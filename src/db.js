db3a9b29670b0c18d8e2db2253641360581447fd
hwidong <gnlehd94613@gmail.com>
2023-07-28 16:55:11 +0900

    Connecting to Mongo

---
+++ src/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/wetube");
+
+
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB 💻");
+const handleError = (error) => console.log("❌ DB Error", error);
+db.on("error", handleError);
+db.once("open", handleOpen);
