e4116aaccf78cfb790b025c72cbef99ab1d47cf4
hwidong <gnlehd94613@gmail.com>
2023-07-31 17:54:01 +0900

    Separating the server.js and init.js files

--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-import "./db";
-import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 
@@ -7,7 +5,6 @@ import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
@@ -19,11 +16,8 @@ app.use("/", globalRouter)
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
-app.listen(PORT, handleListening);
-
-
+export default app
 
 
 
