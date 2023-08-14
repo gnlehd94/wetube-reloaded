8735aacce0f20594a78435d44055cbbf0c11b74c
hwidong <gnlehd94613@gmail.com>
2023-08-14 15:45:07 +0900

    Exceptions and Validation

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -30,17 +30,17 @@ export const getUpload = (req, res) => {
 
 export const postUpload = async(req, res) => {
     const {title, description, hashtags} = req.body;
-    const video = new Video({
+    try {
+    await Video.create({
         title,
         description,
-        createdAt: Date.now(),
         hashtags: hashtags.split(",").map((word) => `#${word}`),
-        meta: {
-            views: 0,
-            rating: 0,
-        }
     })
-    const dbVideo = await video.save();
-    console.log(dbVideo)
+    } catch(error){
+        return res.render("upload", {
+            pageTitle: `Upload Video`,
+            errorMessage: error._message,});
+    }
+
     return res.redirect("/");
 };
