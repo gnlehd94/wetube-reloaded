1ccae758d9b5686a61e94e99d7c53b5518e2e287
hwidong <gnlehd94613@gmail.com>
2023-08-16 16:35:58 +0900

    Search

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -6,7 +6,7 @@ import Video from "../models/Video";
     // });
 
 export const home = async (req, res) => {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({createdAt:"desc"});
     console.log(videos);
     return res.render("home", {pageTitle: "Home", videos})
 };
@@ -67,3 +67,17 @@ export const deleteVideo = async(req, res) => {
     await Video.findByIdAndDelete(id);
     return res.redirect("/");
 } 
+
+export const search = async (req, res) => {
+    const { keyword } = req.query;
+    let videos = [];
+    if (keyword) {
+         videos = await Video.find({
+            title: {
+                $regex: new RegExp(keyword, "i"),
+            },
+        });
+        console.log(videos);
+    }
+    return res.render("search", {pageTitle: `Search`, videos});
+}
